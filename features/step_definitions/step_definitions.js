4b090a1bb9b70e452ddafd1e02862f0b186dc4cb
prasunmitra06 <prasunmitra06@gmail.com>
2019-05-07 02:05:03 +0530

    adding files

--- features/step_definitions/step_definitions.js
+++ features/step_definitions/step_definitions.js
@@ -1,10 +1,15 @@
 const { Given, When, Then } = require('cucumber');
 const got = require('got');
 const assert = require('assert');
+var fs = require('fs');
 
 //// Your Step Definitions /////
 Given(/^send GET request to "([^"]*)", dt should be "([^"]*)"$/, async function (url, name) {
+
+  var parsedJSON = require(name);
+
     let res = await got.get(url);
     let json = JSON.parse(res.body);
-    return assert.equal(json.list[0].dt, name);
+    console.log(parsedJSON);
+    return assert.deepEqual(json, parsedJSON);
 });
